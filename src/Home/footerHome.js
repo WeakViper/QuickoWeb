46d84eee6fb4fe88c00e084fcd2812b10d34ceee
Adeeb Khan <adeeb1037@gmail.com>
2023-08-19 11:15:31 -0700

    merging

--- src/Home/footerHome.js
+++ src/Home/footerHome.js
@@ -1,43 +1,24 @@
-const styles = {
-  footerNav: {
-    position: 'fixed',
-    bottom: 0,
-    right: 0,
-    width: '40%',
-    backgroundColor: '#FA2640',
-    display: 'flex',
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-    padding: '5px 150px',
-    borderTop: '3px solid rgba(255, 255, 255, 0.1)',
-  },
-  footerLogo: {
-    width: '150px',
-    height: '150px',
-  },
-  footerLink: {
-    color: '#fff',
-    textDecoration: 'none',
-    margin: '0 40px',
-    whiteSpace: 'nowrap', // Prevent text from wrapping to a new line
-    
-  },
-  footerLinkHover: {
-    backgroundColor: '#ff4d4d', // Hover background color
-  },
-};
+import React from 'react';
 
-
-
-const FooterNavbar = () => {
+const FooterHome = () => {
   return (
-    <nav style={styles.footerNav} className="footer-nav">
-      <a href="#" className="logo-link">
-        <img src="logo.png" alt="Logo" style={styles.footerLogo} />
-      </a>
-      <a href="#" className="footer-link" style={styles.footerLink}>Terms and Conditions</a>
-      <a href="#" className="footer-link" style={styles.footerLink}>Contact Us</a>
-    </nav>
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-logo">
+          {/* Your logo */}
+          <img src="/path/to/logo.png" alt="Logo" />
+        </div>
+        <div className="footer-links">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+        <div className="footer-text">
+          Centered Text
+        </div>
+      </div>
+    </footer>
   );
 };
-export default FooterNavbar;
+
+export default FooterHome;
--- src/SignInSignUp/signUp.js
+++ src/SignInSignUp/signUp.js
@@ -4,70 +4,50 @@ import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import {
   MDBBtn,
   MDBContainer,
+  MDBRow,
+  MDBCol,
   MDBCard,
   MDBCardBody,
-  MDBCol,
-  MDBRow,
   MDBInput,
-  MDBCheckbox,
-  MDBIcon
-} from 'mdb-react-ui-kit';
+  MDBIcon,
+  MDBCheckbox
+}
+from 'mdb-react-ui-kit';
 
-function Signup() {
+const styles = {
+  body: {
+    backgroundColor: "#FA2640",
+  },
+};
+
+function App() {
   return (
-    <MDBContainer fluid style={{ padding: 0, margin: 0 }}>
-      <MDBRow className='d-flex justify-content-center align-items-center h-100' style={{ backgroundColor: "#FA2640", minHeight: '100vh' }}>
+    <MDBContainer fluid style={styles.body}>
+
+      <MDBRow className='d-flex justify-content-center align-items-center h-100'>
         <MDBCol col='12'>
-          <MDBCard
-            className='bg-white my-5 mx-auto'
-            style={{
-              borderRadius: '1rem',
-              maxWidth: '500px',
-              marginTop: '-100px'
-            }}
-          >
-            <MDBCardBody className='p-5 d-flex flex-column align-items-center mx-auto w-100'>
-            <h2 className="fw-bold mb-2 text-center">
-  <img src="3.png" alt="Your Logo" style={{ maxWidth: '100%', height:'200px' }} />
-</h2>
 
-              <MDBRow>
-                <MDBCol col='6'>
-                  <MDBInput wrapperClass='mb-4' label='First name' id='form1' type='text' />
-                </MDBCol>
+          <MDBCard className='bg-white my-5 mx-auto' style={{borderRadius: '1rem', maxWidth: '500px'}}>
+            <MDBCardBody className='p-5 w-100 d-flex flex-column'>
 
-                <MDBCol col='6'>
-                  <MDBInput wrapperClass='mb-4' label='Last name' id='form1' type='text' />
-                </MDBCol>
-              </MDBRow>
+              <h2 className="fw-bold mb-2 text-center">
+                <img src="3.png" alt="Your Logo" style={{ maxWidth: '100%', height:'200px' }} />
+              </h2>
 
-              <MDBInput wrapperClass='mb-4' label='Email' id='form1' type='email' />
-              <MDBInput wrapperClass='mb-4' label='Password' id='form1' type='password' />
-              <MDBInput wrapperClass='mb-4' label='Confirm Password' id='form1' type='Confirm Password' />
+              <p className="text-white-50 mb-3">Please enter your login and password!</p>
 
-              <MDBInput wrapperClass='mb-4' label='Phone Number' id='form1' type='Phone Number' />
+              <MDBInput wrapperClass='mb-4 w-100' label='Email address' id='formControlLg' type='email' size="lg"/>
+              <MDBInput wrapperClass='mb-4 w-100' label='Password' id='formControlLg' type='password' size="lg"/>
 
+              <MDBCheckbox name='flexCheck' id='flexCheckDefault' className='mb-4' label='Remember password' />
 
-              <div className='d-flex justify-content-center mb-4'>
-                <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Subscribe to our newsletter' />
-              </div>
-
-              <MDBBtn className='w-100 mb-4' size='md'>
-                sign up
+              <MDBBtn size='lg'>
+                Login
               </MDBBtn>
 
-              <div className='text-center'>
-                <p>or sign up with:</p>
+              <hr className="my-4" />
 
-                <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                  <MDBIcon fab icon='facebook-f' size='sm' />
-                </MDBBtn>
-
-                <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                  <MDBIcon fab icon='twitter' size='sm' />
-                </MDBBtn>
-
-                <MDBRow>
+              <MDBRow>
                 <MDBCol col='6'>
                   <MDBBtn className="mb-2 w-100" size="lg" style={{backgroundColor: '#dd4b39'}}>
                     <MDBIcon fab icon="google" className="mx-2"/>
@@ -81,13 +61,15 @@ function Signup() {
                   </MDBBtn>
                 </MDBCol>
               </MDBRow>
-              </div>
+
             </MDBCardBody>
           </MDBCard>
+
         </MDBCol>
       </MDBRow>
+
     </MDBContainer>
   );
 }
 
-export default Signup;
+export default App;
